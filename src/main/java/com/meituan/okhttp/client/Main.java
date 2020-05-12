a71718626f8cca417e31b11a6f43cfc1606dc826
zcz； <zhangchangzhen@meituan.com>
2020-05-12 16:03:19 +0800

    ange

--- src/main/java/com/meituan/okhttp/client/Main.java
+++ src/main/java/com/meituan/okhttp/client/Main.java
@@ -6,12 +6,24 @@ import lombok.extern.slf4j.Slf4j;
 public class Main {
 //    private static Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        log.info("start");
-        OkHttpRequest okHttpRequest = new OkHttpRequest();
-        okHttpRequest.syncGet();
-        okHttpRequest.aSyncGet();
+//        log.info("start");
+//        OkHttpRequest okHttpRequest = new OkHttpRequest();
+//        okHttpRequest.syncGet();
+//        okHttpRequest.aSyncGet();
 
-        log.info("all");
+        int a = 0;
+        try {
+            int b = 1/a;
+        } catch (Exception e) {
+            log.info("all {}",e);
+        }
+
+//        List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5);
+//        StringBuilder stringBuilder = new StringBuilder();
+//        stringBuilder.append(integers);
+//        log.info(stringBuilder.toString());
+        //change
+
 
     }
 }
